f91ec7938de40655f7966f8c8f6fdfdea22c8e3c
Dettlaff <maciekwiatr17@gmail.com>
2020-06-22 20:59:46 +0200

    Fix pwa

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import StoreProvider from "./store";
 ReactDOM.render(
     <React.StrictMode>
         <StoreProvider>
-            <App />
+            <App />a
         </StoreProvider>
     </React.StrictMode>,
     document.getElementById("root")
@@ -17,4 +17,11 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    // dev code
+    serviceWorker.unregister();
+} else {
+    // production code
+    serviceWorker.register();
+}
